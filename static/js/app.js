679a8d7de161d34dc3559a91070f0efad6a5b122
Holly Heredia <hollyheredia@gmail.com>
2021-07-22 15:07:31 -0700

    Update colors.

--- static/js/app.js
+++ static/js/app.js
@@ -1,43 +1,37 @@
+ // Function for change on dropdown menu
 function optionChanged(selectedID){
-    // Check if value is selected
+
+    // Check if value is selected in dropdown
     console.log(selectedID);
-    // Read json file
-    d3.json("data/samples.json").then(data)
-    d3.select(#selDataset).html("");
-    // Select value from dropdown menu
-    data.metadata.forEach(item => {
-    d3.select(#selDataset).append('option').attr('value', item.id).text(item.id);
-    });
-    // Filter Metadata for selected ID from dropdown
-    const idMetadata = data.metadata.filter(item=> (item.id == selectedID));
-    // Check the metadata loaded for the selected ID
-    console.log(idMetadata);
-    
-    // Clear dropdown
+ 
+    // Read the json file for the data
+    d3.json("data/samples.json").then((data) => {
+ 
+    // Clears dropdown
     d3.select("#selDataset").html("");   
     
     // Select the metadata array and for each item append the item ID and adds ID to dropdown
     data.metadata.forEach(item =>
          {
-          // console.log(item.id);
          d3.select ("#selDataset").append('option').attr('value', item.id).text(item.id);
          });
     // Selected value is passed
     d3.select("#selDataset").node().value = selectedID;
+    
     // Filter Metadata for selected ID from dropdown
     const idMetadata = data.metadata.filter(item=> (item.id == selectedID));
-     
+
     // Check the metadata loaded for the selected ID
     console.log(idMetadata);
     
     const panelDisplay = d3.select("#sample-metadata");
     panelDisplay.html("");
     Object.entries(idMetadata[0]).forEach(item=> 
-
-
        {
           panelDisplay.append("p").text(`${item[0]}: ${item[1]}`)
-       });   // BAR CHART
+       });
+ 
+    // BAR CHART
  
     // Filter sample array data for the selected ID
     const idSample = data.samples.filter(item => parseInt(item.id) == selectedID); 
@@ -50,34 +44,33 @@ function optionChanged(selectedID){
     var otuLabels = idSample[0].otu_labels
     otuLabels = otuLabels.reverse();
 
-   // Y axis of bar chart
-   const yAxis = otuID.map(item => 'OTU' + " " + item);
-   console.log(yAxis);
-
-   // Define the layout and trace object, edit color and orientation
-   const trace = {
-   y: yAxis,
-   x: sampleValue,
-   type: 'bar',
-   orientation: "h",
-   text:  otuLabels,
-   marker: {
-      color: 'rgb(154, 140, 152)',
-      line: {
-         width: 3
-   }
-   }
-   },
-   layout = {
-   title: 'Top 10 Operational Taxonomic Units (OTU)/Individual',
-   xaxis: {title: 'Number of Samples Collected'},
-   yaxis: {title: 'OTU ID'}
-   };
-
-   // Plot using Plotly
-   Plotly.newPlot('bar', [trace], layout,  {responsive: true});
-
-   // BUBBLE CHART
+    // Y axis of bar chart
+    const yAxis = otuID.map(item => 'OTU' + " " + item);
+    
+    // Define the layout and trace object, edit color and orientation
+       const trace = {
+       y: yAxis,
+       x: sampleValue,
+       type: 'bar',
+       orientation: "h",
+       text:  otuLabels,
+       marker: {
+          color: 'rgb(41, 128, 185)',
+          line: {
+             width: 3
+         }
+        }
+       },
+       layout = {
+       title: 'Top 10 Operational Taxonomic Units (OTU)/Individual',
+       xaxis: {title: 'Number of Samples Collected'},
+       yaxis: {title: 'OTU ID'}
+       };
+ 
+       // Plot using Plotly
+       Plotly.newPlot('bar', [trace], layout,  {responsive: true});    
+       
+ // BUBBLE CHART
  
  // Remove Sample value and otuID from individual
  var sampleValue1 =idSample[0].sample_values;
@@ -104,64 +97,9 @@ function optionChanged(selectedID){
     width: 1800
     };
     
-   // Plot using Plotly
-   Plotly.newPlot('bubble', [trace1], layout1);
-
-   // BONUS: GAUGE CHART
-
- // Gauge Chart to plot weekly washing frequency 
- const guageDisplay = d3.select("#gauge");
- guageDisplay.html(""); 
- const washFreq = idMetadata[0].wfreq;
- 
- const guageData = [
-    {
-      domain: { x: [0, 1], y: [0, 1] },
-      value: washFreq,
-      title: { text: "<b>Belly Button Washing Frequency </b><br> (Scrubs Per Week)" },
-      type: "indicator",
-      mode: "gauge+number",     
-       gauge: {
-       axis: { range: [0,9] },
-       bar: { color: "#f2e9e4" },
-       steps: [
-          { range: [0, 1], color: "#e5d5d0" },
-          { range: [1, 2], color: "#dbc7c2" },
-          { range: [2, 3], color: "#d2b9b4" },
-          { range: [3, 4], color: "#c9ada7" },
-          { range: [4, 5], color: "#ac9899" },
-          { range: [5, 6], color: "#8a7e88" },
-          { range: [6, 7], color: "#7d7482" },
-          { range: [7, 8], color: "#706a7b" },
-          { range: [8, 9], color: "#4a4e69" }
-                
-        ],
-       threshold: {
-          value: washFreq
-        }
-      }
-    }
-  ]; 
-  const gaugeLayout = {  width: 600, 
-                   height: 400, 
-                   margin: { t: 0, b: 0 }, 
-                    };
- 
  // Plot using Plotly
-  Plotly.newPlot('gauge', guageData, gaugeLayout); 
+ Plotly.newPlot('bubble', [trace1], layout1);
  
- });
- }
- 
- // Initial test starts at ID 940
- optionChanged(940);
- 
- // Event on change takes the value and calls the function during dropdown selection
- d3.select("#selDataset").on('change',() => {
- optionChanged(d3.event.target.value);
- 
- });
-
  // BONUS: GAUGE CHART
 
  // Gauge Chart to plot weekly washing frequency 
